1dd996c81cd6b66431ca6e15e0899012dfb32579
Senior Mosquito <seniormosquito@mailinator.com>
2013-08-18 21:00:15 +0200

    work in progress - finished unit testing framework sample

--- testing.lisp
+++ testing.lisp
@@ -83,3 +83,65 @@
 
 (defmacro where (&rest clauses)
   `#'(lambda (cd) (and ,@(make-comparisons-list clauses))))
+
+(defun primep (number)
+  (when (> number 1)
+    (loop for fac from 2 to (isqrt number) never (zerop (mod number fac)))))
+
+(defun next-prime (number)
+  (loop for n from number when (primep n) return n))
+
+(defmacro do-primes ((var start end) &body body)
+  (let ((ending-value-name (gensym)))
+    `(do ((,var (next-prime ,start) (next-prime (1+ ,var)))
+	  (,ending-value-name ,end))
+	 ((> ,var ending-value))
+       ,@body)))
+
+(defmacro with-gensyms2 ((&rest names) &body body)
+  `(let ,(loop for n in names collect `(,n (gensym)))
+     ,@body))
+
+;; (defun test-+ ()
+;;   (and
+;;    (= (+ 1 2) 3)
+;;    (= (+ 1 2 3) 6)
+;;    (= (+ -1 -3 ) -4)))
+
+(deftest test-+ ()
+    (check 
+      (= (+ 1 2) 3)
+      (= (+ 1 2 3) 6)
+      (= (+ -1 -3 ) -4)
+      (= (+ 1 1) 3)))
+
+(defun report-result (result form)
+  (format t "~:[FAIL~;pass~] ... ~a: ~a~%" result *test-name* form)
+  result)
+  
+(defmacro check (&body forms)
+  `(combine-results
+     ,@(loop for f in forms collect  `(report-result ,f ',f))))
+
+(defmacro combine-results (&body forms)
+  (with-gensyms2 (result)
+    `(let ((,result t))
+       ,@(loop for f in forms collect `(unless ,f (setf ,result nil)))
+       ,result)))
+
+(deftest test-* ()
+    (check
+      (= (* 2 2) 4)
+      (= (* 3 -4) -12)))
+
+(deftest test-arithmetic ()
+  (combine-results
+    (test-+)
+    (test-*)))
+
+(defvar *test-name* nil)
+
+(defmacro deftest (name parameters &body body)
+  `(defun ,name ,parameters  
+     (let ((*test-name* (append *test-name* (list ',name))))
+       ,@body)))
